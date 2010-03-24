f045c6a819b7443e9530573bbd4b019b2a8c1e41
pchambino <pchambino@2b4f7537-2c62-426f-48e4-638bcae4dd43>
2010-03-24 13:30:16 +0000

    Parse e print feitas, mas nao testadas!

---
+++ trunk/V1/cmd.h
@@ -0,0 +1,29 @@
+
+#ifndef _CMD_H_
+#define _CMD_H_
+
+#include <string.h>
+
+typedef struct {
+	char **arg; // lista de argumentos (terminada com NULL)
+	char *infile; // fich. p/onde redireccionar stdin ou NULL	char *outfile; // fich. p/onde redirecc. stdout ou NULL
+	int background;	// 0 ou 1(=proc.o a executar em background)
+} Command_Info ;
+
+/** Recebe uma string que representa um commando
+	e constroi a estrutura Command_Info.
+	
+	@return 0 se cmd_line estiver correcta e -1 em caso de erro
+*/
+int parse_cmd(char *cmd_line, Command_Info *cmd_info);
+
+/**	Imprime o Command_Info para o stdout.
+	Função para teste.
+*/
+void print_cmd(Command_Info *cmd_info);
+
+/** Inicia o prompt da mini shell.
+*/
+void prompt();
+
+#endif // _CMD_H_
