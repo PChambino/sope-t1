1976a2607292a93d4d87e3d4a7494a7c6a7feb63
evamasmarques <evamasmarques@2b4f7537-2c62-426f-48e4-638bcae4dd43>
2010-03-30 16:13:43 +0000

    File structure modified

--- V2/cmd.h
+++ V2/cmd.h
@@ -5,15 +5,23 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-
-#define MAX_PROMPT_LINE 1024 ///< maximo tamanho da linha recebida no prompt
 
 typedef struct {
 	char **arg; ///< lista de argumentos (terminada com NULL)
-	char *infile; ///< fich. p/onde redireccionar stdin ou NULL	char *outfile; ///< fich. p/onde redirecc. stdout ou NULL
+	char *infile; ///< fich. p/onde redireccionar stdin ou NULL
+	char *outfile; ///< fich. p/onde redirecc. stdout ou NULL
 	int background;	///< 0 ou 1(=proc.o a executar em background)
 } Command_Info ;
+
+/** Cria um Command_Info.
+	Usando o tamanho maximo da linha prompt, alocando espaco na
+	memoria para (max_prompt_line / 2 + 1) tokens.
+*/
+Command_Info* newCommand_Info(int max_prompt_line);
+
+/** Liberta a memória usada pelo Command_Info.
+*/
+void deleteCommand_Info(Command_Info *cmd_info);
 
 /** Recebe uma string que representa um commando
 	e constroi a estrutura Command_Info.
@@ -26,19 +34,5 @@ int parse_cmd(char *cmd_line, Command_Info *cmd_info);
 	Função para teste.
 */
 void print_cmd(Command_Info *cmd_info);
-
-/** Inicia o prompt da mini shell.
-	Le os comandos e executa-os se parse_cmd devolver 0.
-	Faz depois um waitpid pelo processo filho.
-	Imprime mensagens de erro relativas 'a syntax ou se nao encontrar um comando.
-*/
-void prompt();
-
-/** Executa o comando.
-	Cria um processo usando fork e exec para executar o comando.
-	
-	@return a PID do processo, ou -1 em caso de erro no fork
-*/
-pid_t exec_simple(Command_Info *cmd_info);
 
 #endif // _CMD_H_
