a16c46750338b2ad8adfdaa5ed24f0317d33c4c7
pchambino <pchambino@2b4f7537-2c62-426f-48e4-638bcae4dd43>
2010-03-30 18:26:04 +0000

    V4 copied from V3

---
+++ V4/exec.c
@@ -0,0 +1,35 @@
+
+#include "exec.h"
+
+pid_t exec_simple(Command_Info *cmd_info) {
+	pid_t child = fork();
+
+	if (child < 0) // child process not created!
+		return -1;
+
+	if (child > 0) // parent returns child pid
+		return child;
+		
+	// only child gets here:
+	
+	// input file
+	if (cmd_info->infile != NULL) {
+		int file = open(cmd_info->infile, O_RDONLY);
+		if (file == -1) // File not found!
+			exit(2);
+		dup2(file, STDIN_FILENO);
+	}
+	
+	// output file
+	if (cmd_info->outfile != NULL) {
+		int file = open(cmd_info->outfile, O_WRONLY | O_CREAT, 0644);
+		dup2(file, STDOUT_FILENO);
+	}
+	
+	// execute command
+	execvp(cmd_info->arg[0], cmd_info->arg);
+	
+	// if it gets here execution failed!
+	exit(1);
+}
+
