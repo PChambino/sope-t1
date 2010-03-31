502193b8c632cc2e8a592c6f65a63ed2c34bf911
pchambino <pchambino@2b4f7537-2c62-426f-48e4-638bcae4dd43>
2010-03-31 15:22:15 +0000

    Print error msg to stderr and fflush to stdout corrected. V4 SIGINT handler working.

--- V4/msh.c
+++ V4/msh.c
@@ -1,22 +1,32 @@
 
 #include "msh.h"
 
+static pid_t child = 0; ///< PID of child process
+
+void sigint_handler(int sig) {
+	if (child != 0)
+		kill(child, SIGINT);
+}
+
 void prompt() {
+	if (signal(SIGINT, sigint_handler) == SIG_ERR) {
+		fprintf(stderr, "SIGINT handler not installed!");
+	}
+	
 	char line[MAX_PROMPT_LINE];
 	Command_Info* cmd_info = newCommand_Info(MAX_PROMPT_LINE);
 	
-	pid_t child = 0;
 	int status = 0;
 
 	while (1) {
-		printf("msh$ ");	fflush(stdin);
+		printf("msh$ ");	fflush(stdout);
 		fgets(line, MAX_PROMPT_LINE, stdin);
 		if (feof(stdin) || ferror(stdin))
 			break;
 
 		if (parse_cmd(line, cmd_info) != 0) {
 			if (!cmd_empty(cmd_info))
-				printf("Syntax error!\n");
+				fprintf(stderr, "Syntax error!\n");
 			continue;
 		}
 			
@@ -25,14 +35,15 @@ void prompt() {
 			
 		if ((child = exec_simple(cmd_info)) > 0) {
 			waitpid(child, &status, 0);
+			child = 0; // reset child to 0 so it knows when to send the signal
 			
 			if (WIFEXITED(status))
 				switch (WEXITSTATUS(status)) {
 					case 1:
-						printf("Command Not Found!\n");
+						fprintf(stderr, "Command Not Found!\n");
 						break;
 					case 2:
-						printf("File Not Found! (%s)\n", cmd_info->infile);
+						fprintf(stderr, "File Not Found! (%s)\n", cmd_info->infile);
 						break;
 					default:
 						break;
